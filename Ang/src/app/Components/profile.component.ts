caf4b9266f85005d6033ba080240397cee1a0af6
Aleksey Sakevich <a.sakevich@dunice.net>
2017-08-04 17:48:52 +0300

    try make create news form on Angular

--- Ang/src/app/Components/profile.component.ts
+++ Ang/src/app/Components/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, DoCheck, OnInit, OnChanges, TemplateRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProfileService } from '../Service/get-profile.service';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
@@ -12,13 +13,24 @@ import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 export class DemoModalServiceStaticComponent {
     public modalRef: BsModalRef;
     constructor(private modalService: BsModalService) { }
-
     public openModal(template: TemplateRef<any>) {
         this.modalRef = this.modalService.show(template);
     }
+    onSubmit() { }
 }
 
-
+export class SimpleFormGroup {
+  public form = new FormGroup({
+    title: new FormControl(),
+    text: new FormControl(),
+  });
+ 
+  onSubmit(): void {
+    console.log(this.form.value);  // {first: 'Nancy', last: 'Drew'}
+  }
+ 
+  setValue() { this.form.setValue({title: 'Carson', text: 'Drew'}); }
+}
 
 
 @Component({
